2a5c9f68af99a5a15a9da4dfcf8a27e7dd30d4fd
piyush5090 <govindanipiyush10@gmail.com>
2025-08-19 21:14:26 +0530

    Update server.js again

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,14 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
+app.use(cors());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
